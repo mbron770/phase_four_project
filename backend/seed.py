11558193dbd47f6cde480603fe555a76794a4c5b
mbron770 <132225936+mbron770@users.noreply.github.com>
2023-08-06 16:22:47 -0400

    Update seed.py

--- backend/seed.py
+++ backend/seed.py
@@ -52,51 +52,6 @@ with app.app_context():
     print('db seeded')
 
     
-
-    # def seed_data():
-    # db.drop_all()
-    # db.create_all()
-    
-    # users = []
-    # products = []
-    
-    # for i in range(10):
-    #     user = User(fname = fake.first_name(), 
-    #     lname = fake.last_name(), 
-    #     username = random_3_digits(), 
-    #     password = random_3_digits(), 
-    #     address = fake.address())
-        
-    #     users.append(user)
-    #     db.session.add(user)
-        
-    # for i in range(10): 
-    #     product = Product(
-    #         product_name = fake.unique.word(),
-    #         product_category = fake.word(),
-    #         price = random_price(),
-    #         product_quantity = fake.random_int(min = 1, max =100) 
-            
-    #     )
-        
-    # products.append(product)
-    # db.session.add(product)
-    
-    
-    # for i in range(10): 
-    #     transaction = Transaction(
-    #         transaction_amount = products[i].price,
-    #         user = users[i], 
-    #         product = products[i]
-    #     )
-        
-    #     db.session.add(transaction)
-    # db.session.commit()
-        
-    # print('db seeded')
-# seed_data()
-# if __name__ == "__main__":
-#     seed_data()
     
     
     
